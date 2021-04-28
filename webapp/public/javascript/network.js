1bda0eb52a4b11c8d96735eab145be8461677fef
kevinconvery <kevinconvery@gmail.com>
2021-04-28 19:33:26 -0400

    expand reservation functionality

--- webapp/public/javascript/network.js
+++ webapp/public/javascript/network.js
@@ -37,11 +37,25 @@ function getAllListings(params) {
   });
 }
 
-function getAllReservations() {
+function getFulfilledReservations() {
   let url = "/api/reservations";
   return $.ajax({
     url,
   });
+}
+
+function getUpcomingReservations() {
+  let url = "/api/reservations/upcoming";
+  return $.ajax({
+    url,
+  });
+}
+
+function getIndividualReservation(reservationId) {
+  let url = `/api/reservations/${reservationId}`
+  return $.ajax({
+    url,
+  })
 }
 
 const submitProperty = function(data) {
@@ -58,5 +72,20 @@ const submitReservation = function(data) {
     method: "POST",
     url: "/api/reservations",
     data,
+  })
+}
+
+const updateReservation = function(data) {
+  return $.ajax({
+    method: "POST",
+    url: `/api/reservations/${data.reservation_id}`,
+    data,
+  })
+}
+
+const deleteReservation = function(id) {
+  return $.ajax({
+    method: "DELETE",
+    url: `/api/reservations/${id}`
   })
 }
