b22fdaa1ee3b9a774b67f2024de87cb2fce5dd63
kevinconvery <kevinconvery@gmail.com>
2021-04-28 19:35:07 -0400

    expand reservation routes

--- webapp/server/apiRoutes.js
+++ webapp/server/apiRoutes.js
@@ -15,13 +15,37 @@ module.exports = function(router, database) {
       res.error("💩");
       return;
     }
-    database.getAllReservations(userId)
+    database.getFulfilledReservations(userId)
     .then(reservations => res.send({reservations}))
     .catch(e => {
       console.error(e);
       res.send(e)
     });
   });
+
+  router.get('/reservations/upcoming', (req, res) => {
+    const userId = req.session.userId;
+    if (!userId) {
+      res.error("💩");
+      return;      
+    }
+    database.getUpcomingReservations(userId)
+    .then(reservations => res.send({ reservations }))
+    .catch(e => {
+      console.error(e);
+      res.send(e);
+    })
+  })
+
+  router.get('/reservations/:reservation_id', (req, res) => {
+    const reservationId = req.params.reservation_id;
+    database.getIndividualReservation(reservationId)
+    .then(reservation => res.send(reservation))
+    .catch(e => {
+      console.error(e);
+      res.send(e);
+    })
+  })
 
   router.post('/properties', (req, res) => {
     const userId = req.session.userId;
@@ -35,6 +59,7 @@ module.exports = function(router, database) {
       });
   });
 
+  // Create a new reservation
   router.post('/reservations', (req, res) => {
     const userId = req.session.userId;
     if (userId) {
@@ -47,6 +72,21 @@ module.exports = function(router, database) {
         res.send(e);
       })
     } 
+  })
+
+  // update an existing reservation
+  router.post('/reservations/:reservationId', (req, res) => {
+    const reservationId = req.params.reservationId;
+    database.updateReservation({...req.body, reservation_id: reservationId})
+    .then(reservation => {
+      res.send(reservation)
+    })
+  })
+
+  // delete a reservation
+  router.delete('/reservations/:reservationId', (req, res) => {
+    const reservationId = req.params.reservationId;
+    database.deleteReservation(reservationId);
   })
 
   return router;
