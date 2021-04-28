3a807c051dabbeed8f4959bcbc913b7225fd902e
kevinconvery <kevinconvery@gmail.com>
2021-04-28 19:30:46 -0400

    modify reservation display functionality

--- webapp/public/javascript/components/header.js
+++ webapp/public/javascript/components/header.js
@@ -46,14 +46,20 @@ $(() => {
   });
 
   $("header").on("click", '.my_reservations_button', function() {
+    views_manager.show('none');
     propertyListings.clearListings();
-    getAllReservations()
+    getFulfilledReservations()
       .then(function(json) {
-        propertyListings.addProperties(json.reservations, true);
+        propertyListings.addProperties(json.reservations, { upcoming: false });
+        getUpcomingReservations()
+        .then(json => {
+          propertyListings.addProperties(json.reservations, { upcoming: true })
+        })
         views_manager.show('listings');
       })
       .catch(error => console.error(error));
   });
+  
   $("header").on("click", '.my_listing_button', function() {
     propertyListings.clearListings();
     getAllListings(`owner_id=${currentUser.id}`)
