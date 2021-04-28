2af67cc40cc723c88c3983878959e2ca3d438e29
kevinconvery <kevinconvery@gmail.com>
2021-04-28 19:32:50 -0400

    build out reserve button functionality

--- webapp/public/javascript/index.js
+++ webapp/public/javascript/index.js
@@ -2,16 +2,9 @@ $(() => {
   getAllListings().then(function( json ) {
     propertyListings.addProperties(json.properties);
     views_manager.show('listings');
-    // new!
-    // have to wait until they're added to add the listeners
-    const listings = $('#property-listings').children();
-    for (let i = 0; i < listings.length; i++) {
-      $(listings[i]).on('click', '.reserve-button', function() {
-        const idSelector = $(this).attr('id');
-        console.log(idSelector);
-        const idData = $(this).attr('id').substring(17);
-        views_manager.show('newReservation', idData);
-      })
-    }
+    $('.reserve-button').on('click', function() {
+      const idData = $(this).attr('id').substring(17);
+      views_manager.show('newReservation', idData);
+    })
   });
 });
