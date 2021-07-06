b6d4eb01ca773ad18256b466e126e4e3757e1c8c
kevinconvery <kevinconvery@gmail.com>
2021-07-06 14:06:13 -0400

    working query

--- queries/01_queries.sql
+++ queries/01_queries.sql
@@ -1,11 +1,3 @@
--- SELECT properties.*, avg(property_reviews.rating) 
--- FROM properties
--- JOIN property_reviews ON properties.id = property_reviews.property_id
--- GROUP BY properties.id, property_reviews.property_id
--- LIMIT 10;
-
--- SELECT avg(rating) as average_rating from property_reviews WHERE property_id = 1;
-
 SELECT properties.id, avg(property_reviews.rating) AS average_rating
 FROM properties
 JOIN property_reviews ON properties.id = property_reviews.property_id
